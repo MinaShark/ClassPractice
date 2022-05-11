536b093833d1a7018a564f9e3b7ed1801faf34fe
Mina Sediqi <mina@Minas-Air.fios-router.home>
2022-05-10 20:20:10 -0400

    This is gitPractice

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -4,9 +4,12 @@ public class GitHubClass {
 	public static void main (String [] args) {
 		
 		
-		int a = 50;
-		int b = 100;
-		System.out.println(a + b);
+		int a = 10;
+		int b = 10;
+		int c = a+b;
+		
+		System.out.println(c);
+		
 	}
 
 }
--- src/GitPractice.java
+++ src/GitPractice.java
@@ -1,5 +1,5 @@
 
-public class NewClass {
+public class GitPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
